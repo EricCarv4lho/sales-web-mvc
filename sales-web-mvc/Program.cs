c047767fee1f3c996aae789d4cc39177be85e4f0
EricCarv4lho <ericcarv4lho@gmail.com>
2025-07-17 21:57:07 -0300

    Implementa criação de departamentos via frontend React e integração com API ASP.NET

--- sales-web-mvc/Program.cs
+++ sales-web-mvc/Program.cs
@@ -2,53 +2,69 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found."), builder => builder.MigrationsAssembly("SalesWebMvc")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("SalesWebMvcContext")
+        ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found."),
+        builder => builder.MigrationsAssembly("SalesWebMvc")
+    )
+);
 
-
-// Add services to the container.
-
+// Injeção de dependências
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 
+// Controllers e Swagger
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// CORS para permitir requisições do React
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") // porta do React dev server
+        policy.WithOrigins("http://localhost:5173") 
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
 
-
 var app = builder.Build();
 
+// Seed do banco (dados iniciais)
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var seedingService = services.GetRequiredService<SeedingService>();
-    seedingService.Seed(); // popula o banco com dados iniciais
+    seedingService.Seed();
 }
 
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Middleware
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();        
+    app.UseSwaggerUI();     
     app.UseHsts();
 }
 
+// Segurança e CORS
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseCors("AllowFrontend");
 
 app.UseRouting();
-app.UseCors("AllowFrontend");
 app.UseAuthorization();
 
+// Mapear Controllers
 app.MapControllers();
+
 app.Run();
