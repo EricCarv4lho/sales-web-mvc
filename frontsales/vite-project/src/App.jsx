29d063453218645d2da7ff683808627b45ffc071
EricCarv4lho <ericcarv4lho@gmail.com>
2025-07-26 10:38:29 -0300

    merge conflict resolvido

--- frontsales/vite-project/src/App.jsx
+++ frontsales/vite-project/src/App.jsx
@@ -5,22 +5,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DepartmentsPage from "./pages/DepartmentsPage";
-<<<<<<< HEAD
 
 import SellersPage from "./pages/SellersPage";
-=======
-import DepartmentsCreate from "./pages/DepartmentsCreate";
->>>>>>> 6737ed392f228d25fde9c7afcce7c83b7f582861
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/departments" element={<DepartmentsPage />} />
-<<<<<<< HEAD
       <Route path="/sellers" element={<SellersPage />} />
-=======
-      <Route path="/departments/create" element={<DepartmentsCreate />} />
->>>>>>> 6737ed392f228d25fde9c7afcce7c83b7f582861
        
     </Routes>
   );
