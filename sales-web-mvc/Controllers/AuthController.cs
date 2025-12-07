5e3ef9b31579d41572f42e9f66e14530b482bf69
EricCarv4lho <ericcarv4lho@gmail.com>
2025-12-06 23:06:01 -0300

    feat(auth): add authcontroller with register and login methods

---
+++ sales-web-mvc/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Dto;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    { 
+        
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest registerRequest)
+        {
+            try
+            {
+                var response = _authService.Register(registerRequest);
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            try
+            {
+                var response = _authService.Login(loginRequest);
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
+           
+        }
+    }
+}
