721ca9e0e4f7982886c4800ed6bb80679d4224e0
EricCarv4lho <ericcarv4lho@gmail.com>
2025-12-06 22:52:34 -0300

    feat: add user entity

--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -4,8 +4,8 @@
     {
         
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
 
---
+++ sales-web-mvc/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class User
+    {
+
+        public User(string email, string password, string role)
+        {
+            
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+
+        public Guid Id { get; set; } 
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User";
+
+    }
+}
