3705d2fa2a0d472968bf5a83ec10545b7714bbcc
EricCarv4lho <ericcarv4lho@gmail.com>
2025-12-07 11:55:54 -0300

    chore: move Vite project contents to frontsales root folder

--- frontsales/src/pages/SalesPage.jsx
+++ frontsales/src/pages/SalesPage.jsx
@@ -3,6 +3,8 @@ import { fetchGroupSales, fetchSales, fetchSimpleSales } from "../services/api";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function SalesPage() {
   const [sales, setSales] = useState([]);
@@ -67,8 +69,69 @@ function SalesPage() {
   };
 
   return (
+    
     <div className="min-h-screen bg-gray-900 p-6 flex flex-col items-center text-center text-white gap-10">
-      <h1 className="text-5xl font-bold">Página de Vendas</h1>
+      <header>
+      <nav className="dark:bg-gray-900 bg-gradient-to-r from-blue-900 via-blue-900 to-blue-1000   fixed w-full top-0 start-0  dark:border-gray-600 shadow-md">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap text-amber-50">
+                    Sales Web MVC
+                  </span>
+      
+                 
+                  <div
+                    className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                    id="navbar-sticky"
+                  >
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                      <li>
+                        <Link to={"/"}>
+                        <a
+                          href=""
+                          className="block py-2 px-3 text-amber-50 rounded-sm hover:bg-gray-100 md:hover:bg-transparent  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                          aria-current="page"
+                        >
+                          Home
+                        </a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to={"/departments"}
+                          className="block py-2 px-3 text-amber-50 rounded-sm hover:bg-gray-100 md:hover:bg-transparent  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        >
+                          Departmentos
+                        </Link>
+                      </li>
+                       <li>
+                        <Link
+                          to={"/sellers"}
+                          className="block py-2 px-3 text-amber-50 rounded-sm hover:bg-gray-100 md:hover:bg-transparent  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        >
+                          Vendedores
+                        </Link>
+                      </li>
+                    <li>
+                          <Link to={"/sales"} className="block py-2 px-3 text-amber-50 rounded-sm hover:bg-gray-100 md:hover:bg-transparent  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+      
+                        
+                          Vendas
+                        </Link>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          className="block py-2 px-3 text-amber-50 rounded-sm hover:bg-gray-100 md:hover:bg-transparent  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        >
+                          Contato
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </header>
+      <h1 className="text-5xl mt-5 font-bold">Página de Vendas</h1>
 
       {/* Filtros */}
       <div className="flex flex-col md:flex-row gap-6">
