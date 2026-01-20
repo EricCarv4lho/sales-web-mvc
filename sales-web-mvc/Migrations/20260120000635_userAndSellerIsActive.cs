be4f365cdb179170591a07cec8df161c45991892
EricCarv4lho <ericcarv4lho@gmail.com>
2026-01-20 00:40:27 -0300

    feat: add migration userandselerisactive

---
+++ sales-web-mvc/Migrations/20260120000635_userAndSellerIsActive.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class userAndSellerIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Seller",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Seller");
+        }
+    }
+}
