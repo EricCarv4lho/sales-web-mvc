2b557a64aace33a83c16944b24b108e443b6b9ec
EricCarv4lho <ericcarv4lho@gmail.com>
2026-01-27 22:25:53 -0300

    feat: add updatesaleaction

---
+++ next-masterclass/app/actions/UpdateSaleAction.tsx
@@ -0,0 +1,42 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+interface SaleUpdateProps {
+    id: number;
+    date: string;
+    amount: number;
+    status: number;
+    sellerId: number;
+}
+
+export default async function UpdateSaleAction(sale: SaleUpdateProps) {
+    try {
+        const cookieStore = await cookies();
+        const token = cookieStore.get("token")?.value;
+
+        if (!token) {
+            return { success: false, message: "Não autenticado" };
+        }
+
+        const response = await fetch(
+            `http://localhost:5225/api/SalesRecords/${sale.id}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(sale),
+            }
+        );
+
+        if (!response.ok) {
+            return { success: false, message: "Erro ao atualizar venda" };
+        }
+
+        return { success: true };
+    } catch (error) {
+        return { success: false, message: "Erro de conexão" };
+    }
+}
